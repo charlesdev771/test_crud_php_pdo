dc2258135a2cd71bfba972d8c3dd2c9ee2fbf046
Carlos Charles Oliveira Dantas <charles.d@academico.ifrn.edu.br>
2023-03-03 15:52:39 -0300

    Add update

--- index.php
+++ index.php
@@ -1,21 +1,37 @@
 <?php
 
-    $pdo = new PDO("mysql:host=localhost;dbname=crudPhp", 'root', '');
 
 
     if (isset($_GET['delete']))
     {
+        $pdo = new PDO("mysql:host=localhost;dbname=crudPhp", 'root', '');
         $id = (int)$_GET['delete'];
         $pdo->exec("DELETE FROM clientes WHERE id=$id");
         echo "Deleted";
     }
 
-    if(isset($_POST['nome']))
+    function insert_db()
     {
-        $sql = $pdo->prepare("INSERT INTO clientes VALUES (null, ?,?)");
-        $sql->execute(array($_POST['nome'], $_POST['email']));
-        echo "Insert!";
+        $pdo = new PDO("mysql:host=localhost;dbname=crudPhp", 'root', '');
+       $sql = $pdo->prepare("INSERT INTO clientes VALUES (null, ?,?)");
+       $sql->execute(array($_POST['nome'], $_POST['email']));
+       echo "Insert!";
     }
+
+   if(isset($_POST['nome']))
+   {
+    insert_db();
+   }
+
+    //if(isset($_GET['update']))
+   //// {
+   //     $nome = $_POST['nome'];
+   //     $email = $_POST['email'];
+    //    $id = $_GET['id'];
+    ///    $sql = $pdo->prepare("UPDATE clientes SET nome = $nome, email = $email WHERE id = 1;");
+    //    $sql->execute(array($_POST['nome'], $_POST['email']));
+    //    echo "Insert!";
+    //}
 
     function update_with_id($id)
     {
@@ -34,6 +50,8 @@
 
 <?php 
 
+$pdo = new PDO("mysql:host=localhost;dbname=crudPhp", 'root', '');
+
     $sql = $pdo->prepare("SELECT * FROM clientes");
     $sql->execute();
 
@@ -41,6 +59,8 @@
 
     foreach ($fetchClientes as $key => $value)
     {
+        echo ' <a href="update.php?update='.$value['id'].'">(update)</a> </a>';
+
         echo '<a href="?delete='.$value['id'].'">(Delete)</a>'.$value['nome'] . ' | ' .$value['email'] . '</a>';
         echo '<hr>';
     }
---
+++ update.php
@@ -0,0 +1,56 @@
+<?php
+
+
+
+    function update_my_table($id)
+    {
+        $pdo = new PDO("mysql:host=localhost;dbname=crudPhp", 'root', '');
+
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $id = $_GET['update'];
+
+        $stmt = $pdo->prepare("UPDATE clientes SET nome=:nome, email=:email WHERE id=:id");
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+
+    if(isset($_POST['nome']))
+    {
+        $id = $_GET['update'];
+        update_my_table($id);
+        echo 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+    }
+
+
+
+
+?>
+
+
+<form method="post">
+    <input type="text" name="nome">
+    <input type="text" name="email">
+    <input type="submit" value="Send">
+</form>
+
+<?php 
+        $pdo = new PDO("mysql:host=localhost;dbname=crudPhp", 'root', '');
+
+    $id = $_GET['update'];
+
+    $sql = $pdo->prepare("SELECT * FROM clientes where id = $id");
+    $sql->execute();
+
+    $fetchClientes = $sql->fetchAll();
+
+    ///foreach ($fetchClientes as $key => $value)
+   // {
+   //     echo '<a href="?update='.$value['id'].'">(update)</a>'.$value['nome'] . ' | ' .$value['email'] . '</a>';
+   //     echo '<hr>';
+   // }
+
+?>
